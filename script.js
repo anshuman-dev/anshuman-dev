6f76a25974dd4ced633a37a36595665e995e12a8
anshuman <singhanshuman216@gmail.com>
2021-12-24 14:48:28 +0530

    Scroll and foot

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
     });
     //toggle menu/navbar
     $('.menu-btn').click(function(){
